dbc5b17561ad11605c088cfc7e8a2b719a3d7053
oliviavc <oliviavcamposano@gmail.com>
2021-12-07 10:26:45 +0800

    add new test data for chromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -12,6 +12,7 @@ public class ResourceCentreTest {
 	private Camcorder cc3;
 	private Chromebook cb1;
 	private Chromebook cb2;
+	Private Chromebook cb3;
 	
 	private ArrayList<Camcorder> camcorderList;
 	private ArrayList<Chromebook> chromebookList;
@@ -28,7 +29,8 @@ public class ResourceCentreTest {
 		cc3 = new Camcorder("CC0013", "panasoni DSC-RX100M7", 30);
 		cb1 = new Chromebook("CB0011", "My Google Chromebook 1st", "Mac OS");
 		cb2 = new Chromebook("CB0012", "SAMSUNG Chromebook 4+", "Win 10");
-
+		cb3 = new Chromebook("CB0013", "HUAWEI Magicbook 4+", "MAC 10");
+		
 		camcorderList= new ArrayList<Camcorder>();
 		chromebookList= new ArrayList<Chromebook>();
 	}
